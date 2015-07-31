22bcf814450f16e8b8e706b6c1c676b4da06a74f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-07-31 20:28:59 +1000

    Remove framework -Swift import

--- CircularButtons/CircularButtons.h
+++ CircularButtons/CircularButtons.h
@@ -15,5 +15,4 @@ FOUNDATION_EXPORT double CircularButtonsVersionNumber;
 FOUNDATION_EXPORT const unsigned char CircularButtonsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CircularButtons/PublicHeader.h>
-#import <CircularButtons/CircularButtons-Swift.h>
 
