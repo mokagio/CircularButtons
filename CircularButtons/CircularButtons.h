213f25a5cd723084b74ef3b9226b5671f475bdfd
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-07-31 19:19:59 +1000

    Derp. What you have to do is create a framework target

---
+++ CircularButtons/CircularButtons.h
@@ -0,0 +1,19 @@
+//
+//  CircularButtons.h
+//  CircularButtons
+//
+//  Created by Giovanni on 31/07/2015.
+//  Copyright (c) 2015 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircularButtons.
+FOUNDATION_EXPORT double CircularButtonsVersionNumber;
+
+//! Project version string for CircularButtons.
+FOUNDATION_EXPORT const unsigned char CircularButtonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircularButtons/PublicHeader.h>
+#import <CircularButtons/CircularButtons-Swift.h>
+
